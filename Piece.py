3e4fae7b7c7eb9fd4e885936eb7b763c847e09c1
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2019-02-03 13:55:31 -0500

    Created Piece Class

---
+++ Piece.py
@@ -0,0 +1,11 @@
+class Piece():
+
+	def __init__(self, colour,position):
+        self.colour=colour
+		self.position=position
+
+    def move(newPosition):
+        raise notimplementederror
+
+    def vaildMove(newPosition):
+        raise notimplementederror
