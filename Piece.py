825ccb627e9667ab48a2be6fa0766731eceb9adb
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2019-02-03 14:22:34 -0500

    Update Piece.py
    Added selfs  , str magic method, added power and name attribute

--- Piece.py
+++ Piece.py
@@ -1,11 +1,22 @@
 class Piece():
 
-	def __init__(self, colour,position):
+    def __init__(self, colour,position,power,name):
         self.colour=colour
-		self.position=position
+        self.position=position
+        self.power=power
+        self.name=name
 
-    def move(newPosition):
+    def move(self,newPosition):
         raise notimplementederror
 
-    def vaildMove(newPosition):
+    def vaildMove(self,newPosition):
         raise notimplementederror
+
+    def __str__(self,powerLevel=False):
+        if powerLevel:
+            return str(self.power)
+        else:
+            if self.colour=="white":
+                return self.name.upper()
+            else:
+                return self.name.lower()
