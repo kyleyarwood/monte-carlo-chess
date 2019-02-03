b84c81c454c7a93887d059a9260bfa22a28d8234
kyleyarwood <35674024+kyleyarwood@users.noreply.github.com>
2019-02-03 16:09:39 -0500

    Updated Piece.py with posn and correct spacing.

--- Piece.py
+++ Piece.py
@@ -1,26 +1,26 @@
 class Piece():
 
-    def __init__(self, colour,position,power,name):
-        self.colour=colour
-        self.position=position
-        self.power=power
-        self.name=name
+    def __init__(self, colour, posn, power, name):
+        self.colour = colour
+        self.posn = posn
+        self.power = power
+        self.name = name
 
-    def move(self,newPosition,board):
-        if vaildMove(self,newPosition,board):
-            self.position=newPosition
+    def move(self, newPosn, board):
+        if vaildMove(self, newPosn, board):
+            self.posn = newPosn
             return True
         else:
             return False
 
-    def vaildMove(self,newPosition,board):
-        raise notimplementederror
+    def vaildMove(self, newPosn, board):
+        raise NotImplementedError
 
-    def __str__(self,powerLevel=False):
+    def __str__(self, powerLevel = False):
         if powerLevel:
             return str(self.power)
         else:
-            if self.colour=="white":
+            if self.colour == "white":
                 return self.name.upper()
             else:
                 return self.name.lower()
